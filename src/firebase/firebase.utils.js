73243def7ba79219f075152ce8d86a06e0fc9863
samaritan <mnshrt2292@gmail.com>
2020-02-05 17:35:50 +0530

    implemented get() instead of collection snapshot

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,10 +16,10 @@ const config =  {
   export const createUserProfileDocument = async(userAuth, additionalData)=>{
     if(!userAuth) return;
     const userRef = firestore.doc(`users/${userAuth.uid}`);
-    const collectionRef = firestore.collection('users'); 
+  //const collectionRef = firestore.collection('users'); 
     
     const snapShot = await userRef.get();
-    const  collectionSnapShot = await collectionRef.get();
+    //const  collectionSnapShot = await collectionRef.get();
 
     //console.log({collection:collectionSnapShot.docs.map(doc=>doc.data())});
     if(!snapShot.exists){
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -18,7 +18,8 @@ class ShopPage extends React.Component {
     componentDidMount(){
       const {updateCollections} = this.props;
       const  collectionRef= firestore.collection('collections');
-      this.unSubscribeFromSnapShot = collectionRef.onSnapshot(async snapshot=>{
+  
+      collectionRef.get().then(snapshot=>{
         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
         updateCollections(collectionsMap);
         this.setState({loading:false});
