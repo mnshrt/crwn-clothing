6a5110793400f6719f1dea4edc7885beedd02fb0
samaritan <mnshrt2292@gmail.com>
2020-02-12 12:18:53 +0530

    initial commit of redux saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,19 @@
 import {createStore, applyMiddleware} from 'redux';
 
 import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
 import {persistStore} from 'redux-persist';
 import rootReducer  from './root-reducer';
 
-const middlewares = [];
+import {fetchCollectionsStart} from '../redux/shop/shop.sagas';
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
 
 if(process.env.NODE_ENV==='development'){
     middlewares.push(logger);
 }
 export const store=createStore(rootReducer, applyMiddleware(...middlewares));
+sagaMiddleware.run(fetchCollectionsStart);
 export const persistor= persistStore(store);
 
 export default { store, persistStore};
